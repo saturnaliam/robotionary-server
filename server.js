6b2ded3e0fa9b2701c0683c63915485ccf8a4e98
saturnaliam <luciacdev@gmail.com>
2024-07-26 20:02:22 -0500

    initial commit :3

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('hello, world :3');
+});
+
+app.listen(port, () => {
+    console.log(`open at localhost:${port}`);
+})
